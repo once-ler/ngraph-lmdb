7271c3da401a4cffded37d965ddde3b84553a31a
htaox <htaox@hotmail.com>
2014-07-06 21:32:37 +0100

    ardb version performs best

--- tests/add-nodes.js
+++ tests/add-nodes.js
@@ -1,13 +1,15 @@
 var g = require('../index')();
 
-var count = 1000; //10K
+var async = require('async');
+
+var count = 10000; //10K
 
 //Create nodes
-var time = process.hrtime(); 
+
 for(var i=0; i < count;i++){
   g.addNode(i+'',{firstName: 'Foo',lastName: 'Bar'});
 }
-console.log('Elapsed: ' + process.hrtime(time));
+
 
 //Create links
 for(var i=0; i < count;i++){  
@@ -17,4 +19,25 @@ for(var i=0; i < count;i++){
   }
 }
 
+var puts = [];
+for(var i=0; i < count;i++){  
+  puts.push(i);
+}
+var time = process.hrtime();
+
+async.eachSeries([0,1,2,3,4,5,6,7,8,9,10], function(q, callback){ 
+  async.each(puts, function(d, next){
+
+    async.each([0,1,2,3,4,5,7],function(e,cb){
+      var n = Math.floor((Math.random() * count));
+      g.addLink(i+'', n+'', 'knows', cb);
+    },function(err){
+      next();
+    })
+  }, function(err){
+    callback();
+  })
+}, function(err){
+  console.log('Elapsed: ' + process.hrtime(time));
+})
 //wget https://raw.githubusercontent.com/rvagg/node-leveldown/master/bench/write-random.js
