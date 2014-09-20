4f06134a3d7620e2c7efaf8558079767c27f603a
htaox <htaox@hotmail.com>
2014-09-20 16:57:14 -0400

    Better looking tests with mocha

---
+++ test/index.js
@@ -0,0 +1,125 @@
+describe('Setup graph', function(){
+
+  var fs = require('fs'),
+    lmdbConfig = require('../lib/lmdb-config');
+
+  if (!fs.existsSync(lmdbConfig.env.path)) {
+    fs.mkdirSync(lmdbConfig.env.path, 0777);
+  }
+
+  var graph = require('../index')();
+  graph.clear();
+
+  decribe('Add 10 nodes & 10 links per node', function(){
+
+    before(function(done){
+
+      var count = 10;
+      for(var i=0; i < count;i++){
+        graph.addNode(i+'', {title: (i % 2 == 0 ? 'Even better' : 'Odd better')});  
+        for (var j=0; j < 10;j++){
+          var n = Math.floor((Math.random() * count));
+          var label = j % 2 == 0 ? 'likes' : 'studies';
+          label = j % 3 == 0 ? 'knows' : label;
+          graph.addLink(i+'', n+'', label);
+        }
+      }
+
+    });
+
+    describe('Count nodes', function(){      
+      it('should be 10', function(done){
+        var nodesCount = graph.getNodesCount(); 
+        nodesCount.should.be.exactly(10);
+        done();
+      });
+    });
+
+    describe('Count links', function(){      
+      it('should be 100', function(done){
+        var linksCount = graph.getLinksCount(); 
+        linksCount.should.be.exactly(100);
+        done();
+      });
+    });
+
+  });
+
+});
+
+describe('Setup shremlin', function(){
+
+  var graph = require('../index')();
+  var shremlin = require('../lib/lmdb-shremlin-wrap');
+  var g = shremlin(graph);
+
+  describe('Get all vertices', function(){
+    it('should get back 10 objects', function(done){
+      var count = 0;
+      g.V().forEach(function(err, d, index, cursor, txn) {
+        if (err){
+          return done(err);
+        }
+        if (!d){
+          return done('Item is undefined or null');
+        }
+        d.should.be.an.Object;
+        count++;        
+      });
+      count.should.be.exactly(10);
+      done();
+    }
+  });
+
+  describe('Get one node by key value', function(){
+    it('should get an object with id == 0', function(done){
+      g.V('0').forEach(function(err, d, index, cursor, txn) {
+        if (err){
+          return done(err);
+        }
+        if (!d){
+          return done('Item is undefined or null');
+        }
+        d.should.be.an.Object.and.containEql({id:'0'});                
+      });
+      done();
+    }
+  });
+
+  describe('Get all nodes with title containing "even"', function(){
+    it('should get 5 objects with title == "even"', function(done){
+      var count = 0;
+      g.V({title:'even'}).forEach(function(err, d, index, cursor, txn) {
+        if (err){
+          return done(err);
+        }
+        if (!d){
+          return done('Item is undefined or null');
+        }
+        d.should.be.an.Object.and.have.ownProperty('title').and.match(/even/);
+        count++;                
+      });
+      count.should.be.exactly(5);
+      done();
+    }
+  });
+
+  describe('Get all nodes with title containing "odd"', function(){
+    it('should get 5 objects with title == "odd"', function(done){
+      var count = 0;
+      g.V({title:'even'}).forEach(function(err, d, index, cursor, txn) {
+        if (err){
+          return done(err);
+        }
+        if (!d){
+          return done('Item is undefined or null');
+        }
+        d.should.be.an.Object.and.have.ownProperty('title').and.match(/odd/);
+        count++;                
+      });
+      count.should.be.exactly(5);
+      done();
+    }
+  });
+
+});
